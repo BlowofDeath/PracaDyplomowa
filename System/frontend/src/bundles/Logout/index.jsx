76e5361cfe1675456fb9fe5c3c6227e6cd051455
Tomasz Jablonski <jabolovp@gmail.com>
2021-04-05 23:31:11 +0200

    Fixing logout

--- System/frontend/src/bundles/Logout/index.jsx
+++ System/frontend/src/bundles/Logout/index.jsx
@@ -4,22 +4,10 @@ import useAuth from "@hooks/useAuth";
 import { useRecoilState } from "recoil";
 import { withApollo } from "@apollo/client/react/hoc";
 
-import {
-  studentAtom,
-  companyAtom,
-  practiceSuperviserAtom,
-} from "@config/userRecoilAtoms";
-
 const Logout = ({ client }) => {
-  const [, setStudent] = useRecoilState(studentAtom);
-  const [, setCompany] = useRecoilState(companyAtom);
-  const [, setPracticeSuperviser] = useRecoilState(practiceSuperviserAtom);
-  setStudent(null);
-  setCompany(null);
-  setPracticeSuperviser(null);
   localStorage.removeItem("token");
   localStorage.removeItem("userType");
-  client.resetStore();
+  client.clearStore();
   return <Redirect to="/login" push={true} />;
 };
 
